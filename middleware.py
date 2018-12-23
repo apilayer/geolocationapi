cad156d6c28ea61d67088ea89804a627b060712d
Madis Väin <madisvain@gmail.com>
2018-12-24 01:48:47 +0200

    debug list

--- middleware.py
+++ middleware.py
@@ -9,8 +9,14 @@ class WWWRedirectMiddleware:
 
     def __call__(self, scope: Scope) -> ASGIInstance:
         if scope["type"] in ("http"):
-            host = scope["headers"].get("host")
+
+            host = None
+            for key, value in scope["headers"]:
+                if key == b"host":
+                    host = value.decode("latin-1")
+                    break
             print(host)
+        
             """
             if host == "ipgeolocationapi.com":
                 url = URL(scope=scope)
